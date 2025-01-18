9c2216c1f073ea132c46f8ff430ba0427681c31c
Lozick <dlozichnyy2005@gmail.com>
2025-01-18 22:26:19 +0700

    add ChatsList

---
+++ src/components/ChatsList/ChatsList.tsx
@@ -0,0 +1,47 @@
+import { useState } from 'react';
+import IconButton from '../../UI/buttons/IconButton/IconButton';
+import ChatInput from '../../UI/Inputs/ChatInput/ChatInput';
+import ChatPreview from '../ChatPreview/ChatPreviw';
+import './chatslist.scss';
+
+const ChatsList = () => {
+  const [searchData, setSearchData] = useState<string>('');
+
+  return (
+    <section className="chats-list">
+      <div className="chats-list__header">
+        <h2 className="chats-list__title">Чаты</h2>
+        <IconButton icon="add_circle" click={() => {}} />
+      </div>
+
+      <ChatInput
+        leftElement={
+          <label htmlFor="search-chat">
+            <span className="material-symbols-outlined">search</span>
+          </label>
+        }
+        id="search-chat"
+        name="search-chat"
+        value={searchData}
+        type="text"
+        change={e => setSearchData(e.target.value)}
+        placeholder="Найти пользователя"
+        required
+      />
+
+      <div className="chats-list__body">
+        {Array.from({ length: 20 }).map((_, idx) => (
+          <ChatPreview
+            key={idx}
+            click={() => {}}
+            avatar="./assets/honey-icon.png"
+            user={`User ${idx + 1}`}
+            message="Hello!"
+          />
+        ))}
+      </div>
+    </section>
+  );
+};
+
+export default ChatsList;
--- src/pages/ChatsPage/ChatsPage.tsx
+++ src/pages/ChatsPage/ChatsPage.tsx
@@ -1,7 +1,16 @@
+import ChatsList from '../../components/ChatsList/ChatsList';
+import OpenChat from '../../components/OpenChat/OpenChat';
 import './chatspage.scss';
 
 const ChatsPage = () => {
-  return <></>;
+  return (
+    <>
+      <main className="chats">
+        <ChatsList />
+        <OpenChat />
+      </main>
+    </>
+  );
 };
 
 export default ChatsPage;
