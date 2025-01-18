f737316e08630fec0c2b2baa1c7a642ff2a303f4
Lozick <dlozichnyy2005@gmail.com>
2025-01-18 22:23:11 +0700

    IconButton, ImgButton

--- src/UI/buttons/Button.ts
+++ src/UI/buttons/Button.ts
@@ -1,8 +1,15 @@
 export interface Button {
   click?: () => void;
-  text: string;
+  text?: string;
 }
 
 export interface ButtonBase extends Button {
   secondary?: boolean;
+}
+export interface ButtonImg extends Button {
+  img: string;
+  color?: string;
+}
+export interface ButtonIcon extends Button {
+  icon: string;
 }
