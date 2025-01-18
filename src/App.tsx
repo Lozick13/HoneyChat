bf59bc5eed159e7c757528ad6918361d13e20ab4
Lozick <dlozichnyy2005@gmail.com>
2025-01-18 22:25:08 +0700

    add Header

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,19 @@
-import { Route, Routes } from 'react-router-dom';
-import './App.scss';
+import { Route, Routes, useLocation } from 'react-router-dom';
+import Header from './components/Header/Header';
 import AuthPage from './pages/AuthPage/AuthPage';
 import ChatsPage from './pages/ChatsPage/ChatsPage';
 import SignUpPage from './pages/SugnUpPage/SignUpPage';
 
 function App() {
+  const location = useLocation();
+
   return (
     <>
+      {!['/auth', '/signup'].includes(location.pathname) && <Header />}
       <Routes>
         <Route path="/auth" element={<AuthPage />} />
         <Route path="/signup" element={<SignUpPage />} />
-        <Route path="/chats" index element={<ChatsPage />} />
+        <Route path="/chats" element={<ChatsPage />} />
       </Routes>
     </>
   );
---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,35 @@
+import ImgButton from '../../UI/buttons/ImgButton/ImgButton';
+import './header.scss';
+
+const Header = () => {
+  return (
+    <>
+      <header className="header">
+        <div className="header__block">
+          <div className="header__logo">
+            <img src="./assets/honey-icon.png" alt="logo" />
+            <h1 className="header__title">HoneyChat</h1>
+          </div>
+          <ImgButton
+            click={() => {}}
+            color="#D68D17"
+            text="Чаты"
+            img="./assets/chats.svg"
+          />
+        </div>
+
+        <div className="header__block">
+          <ImgButton
+            click={() => {}}
+            color="#D68D17"
+            text="Профиль"
+            img="./assets/profile.svg"
+          />
+          <ImgButton click={() => {}} color="#D68D17" img="./assets/log-out.svg" />
+        </div>
+      </header>
+    </>
+  );
+};
+
+export default Header;
