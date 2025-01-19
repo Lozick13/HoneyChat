4055d9aa01f47f7328938cab8711b2516d5dd642
Lozick <dlozichnyy2005@gmail.com>
2025-01-19 16:30:10 +0700

    add OpenChat

--- src/components/OpenChat/OpenChat.tsx
+++ src/components/OpenChat/OpenChat.tsx
@@ -1,7 +1,53 @@
+import { useState } from 'react';
+import IconButton from '../../UI/buttons/IconButton/IconButton';
+import ChatInput from '../../UI/Inputs/ChatInput/ChatInput';
+import ChatMessage from '../ChatMessage/ChatMessage';
 import './openchat.scss';
 
 const OpenChat = () => {
-  return <></>;
+  const [message, setMessage] = useState<string>('');
+
+  return (
+    <section className="open-chat">
+      <h2 className="open-chat__title">Name</h2>
+
+      <div className="open-chat__body">
+        <ChatMessage avatar="./assets/honey-icon.png" user="User" message="Hello!" />
+        <ChatMessage
+          avatar="./assets/honey-icon.png"
+          user="User"
+          message="Hefddsljfgdsfiojgoj;dfsgjldfgjidfgdfgllo!"
+          owner
+        />
+        {/* <ChatMessage avatar="./assets/honey-icon.png" user="User" message="Hello!" />
+        <ChatMessage
+          avatar="./assets/honey-icon.png"
+          user="User"
+          message="Hello!"
+          owner
+        />
+        <ChatMessage avatar="./assets/honey-icon.png" user="User" message="Hello!" />
+        <ChatMessage
+          avatar="./assets/honey-icon.png"
+          user="User"
+          message="Hello!"
+          owner
+        /> */}
+      </div>
+
+      <ChatInput
+        rightElement={<IconButton icon="send" click={() => {}} />}
+        id="message"
+        name="message"
+        value={message}
+        type="text"
+        change={e => setMessage(e.target.value)}
+        placeholder="Напишите Ваше сообщение"
+        required
+        textarea
+      />
+    </section>
+  );
 };
 
 export default OpenChat;
