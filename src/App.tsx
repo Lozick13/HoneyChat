bfb2c458561e130a871d096989973f5b8b37df32
Lozick <dlozichnyy2005@gmail.com>
2025-01-20 20:14:27 +0700

    token check

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,42 @@
-import { Route, Routes, useLocation } from 'react-router-dom';
+import { useEffect } from 'react';
+import { Navigate, Route, Routes, useLocation } from 'react-router-dom';
 import Header from './components/Header/Header';
+import { isTokenValid } from './helpers/token';
+import { useAppDispatch } from './hooks';
 import AuthPage from './pages/AuthPage/AuthPage';
 import ChatsPage from './pages/ChatsPage/ChatsPage';
 import SignUpPage from './pages/SugnUpPage/SignUpPage';
+import { setUser } from './redux/slices/userSlice';
 
 function App() {
+  const dispatch = useAppDispatch();
   const location = useLocation();
+
+  // token validation, get data
+  const token = localStorage.getItem('token');
+  const { valid: tokenIsValid, data: tokenData } = isTokenValid(token);
+
+  // if the token is valid and contains data, set the user to Redux
+  useEffect(() => {
+    if (tokenIsValid && tokenData) {
+      const { id, name, email, chats } = tokenData;
+      dispatch(setUser({ id, name, email, chats }));
+    }
+  }, [tokenIsValid, tokenData, dispatch]);
+
+  // authorization check
+  const requireAuth = (children: React.ReactNode) => {
+    return tokenIsValid ? children : <Navigate to="/auth" replace />;
+  };
 
   return (
     <>
       {!['/auth', '/signup'].includes(location.pathname) && <Header />}
       <Routes>
+        <Route path="/" element={<Navigate to="/chats" replace />} />
         <Route path="/auth" element={<AuthPage />} />
         <Route path="/signup" element={<SignUpPage />} />
-        <Route path="/chats" element={<ChatsPage />} />
+        <Route path="/chats" element={requireAuth(<ChatsPage />)} />
       </Routes>
     </>
   );
---
+++ src/helpers/token.ts
@@ -0,0 +1,33 @@
+import { jwtDecode } from 'jwt-decode';
+
+export interface DecodedToken {
+  exp: number;
+  id: string;
+  name: string;
+  email: string;
+  chats: string[];
+}
+
+// validation check
+export const isTokenValid = (
+  token: string | null,
+): { valid: boolean; data?: DecodedToken } => {
+  if (!token) return { valid: false };
+
+  try {
+    const decoded: DecodedToken = jwtDecode(token);
+    const currentTime = Date.now() / 1000;
+    const isValid = decoded.exp > currentTime;
+
+    return { valid: isValid, data: isValid ? decoded : undefined };
+  } catch (error) {
+    console.error('Invalid token:', error);
+    return { valid: false };
+  }
+};
+
+// decode token
+export const decodedToken = (token: string): DecodedToken => {
+  const decoded: DecodedToken = jwtDecode(token);
+  return decoded;
+};
