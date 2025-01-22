56800a936ef8cca20e6544dd523f2d852265930d
Lozick <dlozichnyy2005@gmail.com>
2025-01-22 14:59:34 +0700

    add avatars

---
+++ src/api/icons.ts
@@ -0,0 +1,11 @@
+export const icons = [
+  './assets/honeybee-icon.png',
+  './assets/eyes-icon.png',
+  './assets/clown-icon.png',
+  './assets/honey-icon.png',
+  './assets/nesting-dolls-icon.png',
+  './assets/pretzel-icon.png',
+  './assets/skull-icon.png',
+  './assets/snail-icon.png',
+  './assets/teddy-bear-icon.png',
+];
