4033283c60cfab264570afb0b57d9e08ac6886e5
Lozick <dlozichnyy2005@gmail.com>
2025-01-24 12:19:58 +0700

    add end.PATH

--- src/api/socketIo.ts
+++ src/api/socketIo.ts
@@ -1,4 +1,4 @@
 import { io } from 'socket.io-client';
 
-export const URL = process.env.PORT || 'http://localhost:3000';
+export const URL = process.env.PATH || 'http://localhost:3000';
 export const socket = io(URL);
